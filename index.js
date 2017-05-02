f02af42a16836bfef1019bd04ec5da51d1d65a80
JJ <jgg3295@gmail.com>
2017-05-02 11:19:47 -0700

    testing game

--- index.js
+++ index.js
@@ -1,27 +1,96 @@
-#!/usr/bin/env node
+var read = require('readline-sync');
 
-var program = require('commander');
+var ticTac = function() {
+  this.moves = [[1, 2, 3], 
+                [4, 5, 6], 
+                [7, 8, 9]];
+  this.count = 0;
+  this.currentPlayer = 'Player 1';
+}
 
-var tic = function() {
-  if (options.start) {
-  	console.log('\n' + 
-      ' 1 | 2 | 3 \n' +
-      '-----------\n' +
-      ' 4 | 5 | 6 \n' +
-      '-----------\n' +
-      ' 7 | 8 | 9 \n'
-  	);
+ticTac.prototype.drawGame = function() {
+  console.log('\n' + 
+    ' ' + this.moves[0][0] + ' | ' + this.moves[0][1] + ' | ' + this.moves[0][2] + ' \n' +
+    '-----------\n' +
+    ' ' + this.moves[1][0] + ' | ' + this.moves[1][1] + ' | ' + this.moves[1][2] + ' \n' +
+    '-----------\n' +
+    ' ' + this.moves[2][0] + ' | ' + this.moves[2][1] + ' | ' + this.moves[2][2] + ' \n'
+  );
+}
+
+ticTac.prototype.getPosition = function(move) {
+  var row = Math.ceil(move / 3) - 1;
+  var col = (move - 1) % 3;
+  return [row, col];
+}
+
+ticTac.prototype.getWinner = function(position) {
+  var row = position[0];
+  var col = position[1];
+  if (this.moves[row][0] === this.moves[row][1] && this.moves[row][1] === this.moves[row][2]) {
+    return true;
+  } else if (this.moves[0][col] === this.moves[1][col] && this.moves[1][col] === this.moves[2][col]) {
+    return true;
+  } else if (this.moves[0][0] === this.moves[1][1] && this.moves[1][1] === this.moves[2][2]) {
+    return true;
+  } else if (this.moves[2][0] === this.moves[1][1] && this.moves[1][1] === this.moves[0][2]) {
+    return true;
   }
-};
+  return false;
+}
 
-program
-  .version('0.0.1')
-  .command('tictac')
-  .description('starts game of tic tac toe')
-  .option('-s, --start', 'start game')
-  .option('-e, --end', 'end game')
-  .option('-f, --first', 'first player plays')
-  .option('-s, --second', 'second player plays')
-  .action('');
+ticTac.prototype.checkForTie = function() {
+  var count = 0;
+  for (var i = 0; i < this.moves.length; i++) {
+    for (var j = 0; j < this.moves[i].length; j++) {
+      if (this.moves[i][j] === 'x' || this.moves[i][j] === 'o') {
+        count++;
+      }
+    }
+  }
+  return count === (this.moves.length * 3);
+}
 
-program.parse(process.argv);
+ticTac.prototype.getMove = function() {
+  var move = read.question(this.currentPlayer + ' enter move from 1-9');
+  if (this.getPosition(move) === 'x' || this.getPosition(move) === 'o') {
+    console.log('Spot is taken. Choose another move');
+    this.getMove();
+  } else if (!(move > 0) || !(move < 10)) {
+    console.log('move must be a number from 1-9');
+    this.getMove();
+  } else {
+    return move;
+  }
+}
+
+ticTac.prototype.switchPlayer = function() {
+  if (this.currentPlayer === 'Player 1') {
+    this.currentPlayer = 'Player 2';
+  } else {
+    this.currentPlayer = 'Player 1';
+  }
+}
+
+ticTac.prototype.start = function() {
+  this.drawGame();
+  var move = this.getMove();
+  var position = this.getPosition(move);
+  if (this.currentPlayer === 'Player 1') {
+    this.moves[position[0]][position[1]] = 'x';
+  } else {
+    this.moves[position[0]][position[1]] = 'o';
+  }
+  if (this.getWinner(position)) {
+    console.log(this.currentPlayer + ' wins!');
+  } else if (this.checkForTie()) {
+    console.log('This game is a tie!');
+  } else {
+    this.switchPlayer();
+    this.start();
+  }
+}
+
+var game = new ticTac();
+game.start();
+
